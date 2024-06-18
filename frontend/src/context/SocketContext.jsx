d317fb943d2b0be341b16d82362d3d659b659e92
Sai Ram <sairamreddyaleti99@gmail.com>
2024-06-18 22:13:33 +0530

    Deployment

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-yt.onrender.com", {
+			const socket = io("https://chat-messanger.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
